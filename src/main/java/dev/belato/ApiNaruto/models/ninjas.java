ee6099620e7d228acf2900f3c070efc7e761502f
LSB30 <lukinhaasantana777@gmail.com>
2024-12-30 14:49:36 -0300

    new brach

---
+++ src/main/java/dev/belato/ApiNaruto/models/ninjas.java
@@ -0,0 +1,22 @@
+package dev.belato.ApiNaruto.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ninjas")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ninjas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    private String name;
+    private int age;
+    private String village;
+    private String element;
+    private String imgUrl;
+}
---
+++ src/main/java/dev/belato/ApiNaruto/repository/ninjasRepository.java
@@ -0,0 +1,7 @@
+package dev.belato.ApiNaruto.repository;
+
+import dev.belato.ApiNaruto.models.ninjas;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ninjasRepository extends JpaRepository<ninjas,Long> {
+}
